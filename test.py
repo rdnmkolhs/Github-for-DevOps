d0fe1d51da9e2989024037e35454535d2ebc848a
Shlok_Mndr <shlokmanandhar5@gmail.com>
2025-07-24 16:38:16 +0545

    BumbelBEE

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
-"""
-This module demonstrates a simple function to add two integers.
-"""
-
-
-def add_numbers():
-    """
-    Initializes two integer variables and returns their sum.
-
-    Returns:
-        int: Sum of two integers.
-    """
-    a = 5
-    b = 10
-    return a + b
-
-
-result = add_numbers()
-print(result)
+a=2
+b=2
+c= a+b
+print(c) 
